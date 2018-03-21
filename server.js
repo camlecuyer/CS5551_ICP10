e059c0097ad247a607db2f3a5fe699c3a9cbb6cc
Cameron <camlecuyer@yahoo.com>
2018-03-21 17:31:08 -0500

    post update

--- server.js
+++ server.js
@@ -119,4 +119,4 @@ app.post('/api/*', function (req, res) {
 app.listen(port, function() {
     console.log('app running')
 })
-console.log('Client Server running at http://127.0.0.1:8081/');
+console.log('Client Server running at http://127.0.0.1:8080/');
